889db41a9804b2c52d1bb28e850b9c72c1200dd1
Karol Walasek <coreconviction@gmail.com>
2018-08-29 20:20:50 +0200

    Fixed timeout tests

---
+++ tests/timeout.test.js
@@ -0,0 +1,54 @@
+const test = require('zora');
+const Timeout = require('../timeout');
+
+module.exports = async function(test){
+	test('Timeout gate', async (t) => {
+		t.test('Timeout gate can handle timeouts', async (t) => {
+			await new Promise((res, rej) => {
+				const txo = setTimeout(() => {
+					rej('Should never be called');
+				}, 1000);
+				const gate = new Timeout(500, () => {
+					clearTimeout(txo);
+				});
+				setTimeout(res, 2000);
+			});
+		});
+		t.test('Timeout can be dismissed', async (t) => {
+			await new Promise((res, rej) => {
+				const gate = new Timeout(1000, () => {
+					rej('Should never be called');
+				});
+				const txo = setTimeout(() => {
+					gate.dismiss();
+				}, 500);
+				setTimeout(res, 2000);
+			});
+		});
+		t.test('Timeout enterGate works properly before timeout', async (t) => {
+			await new Promise((res, rej) => {
+				const gate = new Timeout(1000, () => {
+					rej('Should never be called');
+				});
+				setTimeout(() => {
+					if(!gate.enterGate()){
+						rej('Should never be called');
+					}
+				});
+				setTimeout(res, 2000);
+			});
+		});
+		t.test('Timeout enterGate works properly after timeout', async (t) => {
+			await new Promise((res, rej) => {
+				const gate = new Timeout(500, () => {});
+				setTimeout(() => {
+					if(!gate.enterGate()){
+						res();
+					}
+					rej('Should never be called');
+				}, 1000);
+			});
+		});
+	});
+}
+
